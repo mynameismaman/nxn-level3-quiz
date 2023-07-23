64c9af56424cd909d8db8b6241f11bbed1b89610
mynameismaman <mnrahman940814@gmail.com>
2023-07-23 07:16:54 +0700

    Tugas nxn Level 3

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hsi-sandbox.vercel.app',
+        port: '',
+        pathname: '/image/**',
+      },
+    ],
+  },
+}
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import Style from "../styles/style.css"
+
+export default function MyApp({Component, pageProps}){
+	return <Component {...pageProps} />
+}
